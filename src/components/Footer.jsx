35b7cfb6bbf3d46e707b09c213aa8d75213efd81
Stepan Dordiai <stepandordiai@gmail.com>
2025-08-18 10:27:50 +0200

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,27 @@
+import arrowTopIcon from "/top.png";
 import "./Footer.scss";
 
 const Footer = () => {
+	const handleScrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
+
 	return (
 		<footer className="footer">
+			<button onClick={handleScrollToTop} className="footer__btn">
+				<span>Повернутись на початок</span>
+				<img src={arrowTopIcon} width={15} alt="" />
+			</button>
 			<p>
-				Site by{" "}
-				<a href="https://heeeyooo.studio/" target="_blank">
+				Site by
+				<a
+					className="creator__link"
+					href="https://heeeyooo.studio/"
+					target="_blank"
+				>
 					heeeyooo studio
 				</a>
 			</p>
