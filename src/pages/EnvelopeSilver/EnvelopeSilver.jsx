3b2d1bd514dc24992bbe9d5ccc82f21a13cd304d
Stepan Dordiai <stepandordiai@gmail.com>
2025-08-31 22:13:57 +0200

    .

--- src/pages/EnvelopeSilver/EnvelopeSilver.jsx
+++ src/pages/EnvelopeSilver/EnvelopeSilver.jsx
@@ -4,6 +4,12 @@ import img from "/corner.svg";
 import img2 from "/underline.svg";
 import heartIcon from "/heart2.png";
 import data from "./../../assets/data/data.json";
+import envelopeBase from "/envelope/envelope-base.svg";
+import envelopeRight from "/envelope/envelope-right.svg";
+import envelopeLeft from "/envelope/envelope-left.svg";
+import envelopeBottom from "/envelope/envelope-bottom.svg";
+import envelopeTop from "/envelope/envelope-top.svg";
+import card from "/card.png";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -183,179 +189,214 @@ const EnvelopeSilver = () => {
 			}
 		});
 	}, []);
-	return (
-		<main className="envelope-silver">
-			<div className="envelope-silver__top frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
 
-				<div className="envelope-silver__top-logo animated-element">
-					<span>{envelope.name_1[0]}</span>
-					<span>{envelope.name_2[0]}</span>
+	const handleEnvelope = (e) => {
+		e.currentTarget.classList.add("envelope-inner--active");
+		document.querySelector(".env-top").classList.add("env-top--active");
+		document.querySelector(".card").classList.add("card--active");
+		document.querySelector(".envelope-txt").style.display = "none";
+
+		setTimeout(() => {
+			document.querySelector(".envelope").classList.add("envelope--active");
+			document.body.style.overflow = "auto";
+		}, 4000);
+	};
+
+	return (
+		<>
+			<div className="envelope">
+				<div onClick={handleEnvelope} className="envelope-inner">
+					<img className="env-base" src={envelopeBase} alt="" />
+					<img className="env-bottom" src={envelopeBottom} alt="" />
+					<img className="env-left" src={envelopeLeft} alt="" />
+					<img className="env-right" src={envelopeRight} alt="" />
+					<img className="env-top" src={envelopeTop} alt="" />
+					<img className="card" src={card} alt="" />
 				</div>
-				{/* <div className="divider"></div> */}
-				{/* <div className="home__top-date">
+				<div className="envelope-txt">
+					Натисніть на конверт, щоб відкрити запрошення!
+				</div>
+			</div>
+			<main className="envelope-silver">
+				<div className="envelope-silver__top frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+
+					<div className="envelope-silver__top-logo animated-element">
+						<span>{envelope.name_1[0]}</span>
+						<span>{envelope.name_2[0]}</span>
+					</div>
+					{/* <div className="divider"></div> */}
+					{/* <div className="home__top-date">
 						<p>{envelope.date}</p>
 						<span>/</span>
 						<p>{envelope.month.toString().padStart(2, 0)}</p>
 						<span>/</span>
 						<p>{envelope.year.toString().slice(2)}</p>
 					</div> */}
-				{/* <div> */}
-				{/* <div> */}
-				<div className="envelope-silver__top-title animated-element">
-					<span>{envelope.name_1}</span>
-					<span>та</span>
-					<span>{envelope.name_2}</span>
-				</div>
-				{/* <img className="underline" src={img2} alt="" /> */}
-				{/* </div> */}
-				<p className="envelope-silver__top-date animated-element">
-					Неділя, 14 Вересня, 2025
-				</p>
-				<div className="font scroll animated-element">
-					Прокрутіть вниз, щоб дізнатися більше
-				</div>
-			</div>
-			<div className="frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
-				<div className="frame-inner">
-					<p className="m-size animated-element">Дорогі гості</p>
-					<p className="desc font animated-element">
-						Ми надзвичайно раді поділитися з Вами цим особливим днем!
-						<br />
-						<br />
-						Розпочинаючи нашу спільну подорож, ми будемо щасливі, якщо Ви
-						приєднаєтеся до святкування нашого весілля.
-						<br />
-						<br />
-						Тут ви знайдете всю необхідну інформацію - розклад подій, зворотний
-						відлік, інформацію про місце проведення, галерею та інше.
-						<br />
-						<br />
-						Ваша присутність для нас безцінна, і ми з нетерпінням чекаємо, щоб
-						розділити радість, сміх і любов цього дня.
-						<br />
-						<br />
+					{/* <div> */}
+					{/* <div> */}
+					<div className="envelope-silver__top-title animated-element">
+						<span>{envelope.name_1}</span>
+						<span>та</span>
+						<span>{envelope.name_2}</span>
+					</div>
+					{/* <img className="underline" src={img2} alt="" /> */}
+					{/* </div> */}
+					<p className="envelope-silver__top-date animated-element">
+						Неділя, 14 Вересня, 2025
 					</p>
-					<div className="silver-calendar-wrapper animated-element">
-						<p className="silver-calendar-top">{`${txtMonth} ${envelope.year}`}</p>
-						<div className="silver-calendar">
-							<div>Пн</div>
-							<div>Вт</div>
-							<div>Ср</div>
-							<div>Чт</div>
-							<div>Пт</div>
-							<div>Сб</div>
-							<div>Нд</div>
-							{days2.map((day, index) => {
+					<div className="font scroll animated-element">
+						Прокрутіть вниз, щоб дізнатися більше
+					</div>
+				</div>
+				<div className="frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+					<div className="frame-inner">
+						<p className="m-size animated-element">Дорогі гості</p>
+						<p className="desc font animated-element">
+							Ми надзвичайно раді поділитися з Вами цим особливим днем!
+							<br />
+							<br />
+							Розпочинаючи нашу спільну подорож, ми будемо щасливі, якщо Ви
+							приєднаєтеся до святкування нашого весілля.
+							<br />
+							<br />
+							Тут ви знайдете всю необхідну інформацію - розклад подій,
+							зворотний відлік, інформацію про місце проведення, галерею та
+							інше.
+							<br />
+							<br />
+							Ваша присутність для нас безцінна, і ми з нетерпінням чекаємо, щоб
+							розділити радість, сміх і любов цього дня.
+							<br />
+							<br />
+						</p>
+						<div className="silver-calendar-wrapper animated-element">
+							<p className="silver-calendar-top">{`${txtMonth} ${envelope.year}`}</p>
+							<div className="silver-calendar">
+								<div>Пн</div>
+								<div>Вт</div>
+								<div>Ср</div>
+								<div>Чт</div>
+								<div>Пт</div>
+								<div>Сб</div>
+								<div>Нд</div>
+								{days2.map((day, index) => {
+									return (
+										<div
+											key={index}
+											className={day === envelope.date ? "target-time" : ""}
+										>
+											{day}
+											{day === envelope.date && (
+												<img
+													className="calendar-heart"
+													src={heartIcon}
+													alt=""
+												/>
+											)}
+										</div>
+									);
+								})}
+							</div>
+						</div>
+						<p className="s-size animated-element">
+							Давайте створимо спогади, які залишаться на все життя!
+						</p>
+					</div>
+				</div>
+				<div className="frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+					<div className="frame-inner">
+						<p className="m-size animated-element">
+							Зворотний відлік до нашого Великого дня
+						</p>
+						<div className="font silver-date animated-element" id="date">
+							<div>
+								<span>{days}</span>
+								<span>днів</span>
+							</div>
+							<div>
+								<span>{hours}</span>
+								<span>годин(а)</span>
+							</div>
+							<div>
+								<span>{minutes}</span>
+								<span>хвилин(а)</span>
+							</div>
+							<div>
+								<span>{seconds}</span>
+								<span>секунд(а)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+					<div className="frame-inner">
+						<div>
+							<p className="m-size animated-element">Адреси святкування</p>
+							<p className="font animated-element">{`(місцевий час, ${envelope.place})`}</p>
+						</div>
+						<div className="silver-addresses">
+							{envelope.adresess.map((address, index) => {
 								return (
-									<div
-										key={index}
-										className={day === envelope.date ? "target-time" : ""}
-									>
-										{day}
-										{day === envelope.date && (
-											<img className="calendar-heart" src={heartIcon} alt="" />
-										)}
+									<div key={index} className="silver-address">
+										<p
+											style={{
+												display: "flex",
+												justifyContent: "space-between",
+											}}
+											className="s-size animated-element"
+										>
+											<span>{address.title}</span>
+											<span>{address.time}</span>
+										</p>
+										<br />
+										<p className="font animated-element">
+											{address.address_title}
+										</p>
+										<br />
+										<p
+											style={{ marginBottom: 10 }}
+											className="font animated-element"
+										>
+											{address.address}
+										</p>
+										<br />
+										<iframe
+											className="silver-map animated-element"
+											src={address.address_url}
+											loading="lazy"
+										></iframe>
+										<br />
+										<a
+											className="silver-address__link animated-element"
+											href={address.address_destination_url}
+											target="_blank"
+										>
+											Отримати маршрут
+										</a>
 									</div>
 								);
 							})}
 						</div>
 					</div>
-					<p className="s-size animated-element">
-						Давайте створимо спогади, які залишаться на все життя!
-					</p>
 				</div>
-			</div>
-			<div className="frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
-				<div className="frame-inner">
-					<p className="m-size animated-element">
-						Зворотний відлік до нашого Великого дня
-					</p>
-					<div className="font silver-date animated-element" id="date">
-						<div>
-							<span>{days}</span>
-							<span>днів</span>
-						</div>
-						<div>
-							<span>{hours}</span>
-							<span>годин(а)</span>
-						</div>
-						<div>
-							<span>{minutes}</span>
-							<span>хвилин(а)</span>
-						</div>
-						<div>
-							<span>{seconds}</span>
-							<span>секунд(а)</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
-				<div className="frame-inner">
-					<div>
-						<p className="m-size animated-element">Адреси святкування</p>
-						<p className="font animated-element">{`(місцевий час, ${envelope.place})`}</p>
-					</div>
-					<div className="silver-addresses">
-						{envelope.adresess.map((address, index) => {
-							return (
-								<div key={index} className="silver-address">
-									<p
-										style={{ display: "flex", justifyContent: "space-between" }}
-										className="s-size animated-element"
-									>
-										<span>{address.title}</span>
-										<span>{address.time}</span>
-									</p>
-									<br />
-									<p className="font animated-element">
-										{address.address_title}
-									</p>
-									<br />
-									<p
-										style={{ marginBottom: 10 }}
-										className="font animated-element"
-									>
-										{address.address}
-									</p>
-									<br />
-									<iframe
-										className="silver-map animated-element"
-										src={address.address_url}
-										loading="lazy"
-									></iframe>
-									<br />
-									<a
-										className="silver-address__link animated-element"
-										href={address.address_destination_url}
-										target="_blank"
-									>
-										Отримати маршрут
-									</a>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</div>
 
-			{/* <div className="colors-container">
+				{/* <div className="colors-container">
 				<p className="page-title">Дрес-код</p>
 				<div>
 					<p className="pepe">
@@ -368,50 +409,51 @@ const EnvelopeSilver = () => {
 					</div>
 				</div>
 			</div> */}
-			<div className="frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
-				<div className="frame-inner">
-					<p className="m-size animated-element">Галерея</p>
-					<Swiper
-						effect={"fade"}
-						loop={true}
-						speed={1000}
-						autoplay={{
-							delay: 3000,
-							disableOnInteraction: false,
-						}}
-						modules={[EffectFade, Autoplay]}
-						className="silver-swiper animated-element"
-					>
-						{envelope.gallery.map((img, index) => {
-							return (
-								<SwiperSlide key={index} className="slide">
-									<img src={img} alt="" loading="lazy" />
-								</SwiperSlide>
-							);
-						})}
-					</Swiper>
-				</div>
-			</div>
-			<div className="frame">
-				<img className="corner-top-right" src={img} alt="" />
-				<img className="corner-bottom-right" src={img} alt="" />
-				<img className="corner-bottom-left" src={img} alt="" />
-				<img className="corner-top-left" src={img} alt="" />
-				<div className="frame-inner">
-					<p className="m-size animated-element">
-						Ми з нетерпінням чекаємо, щоб відсвяткувати разом з вами!
-					</p>
-					<div className="envelope-silver__top-logo animated-element">
-						<span>{envelope.name_1[0]}</span>
-						<span>{envelope.name_2[0]}</span>
+				<div className="frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+					<div className="frame-inner">
+						<p className="m-size animated-element">Галерея</p>
+						<Swiper
+							effect={"fade"}
+							loop={true}
+							speed={1000}
+							autoplay={{
+								delay: 3000,
+								disableOnInteraction: false,
+							}}
+							modules={[EffectFade, Autoplay]}
+							className="silver-swiper animated-element"
+						>
+							{envelope.gallery.map((img, index) => {
+								return (
+									<SwiperSlide key={index} className="slide">
+										<img src={img} alt="" loading="lazy" />
+									</SwiperSlide>
+								);
+							})}
+						</Swiper>
 					</div>
 				</div>
-			</div>
-		</main>
+				<div className="frame">
+					<img className="corner-top-right" src={img} alt="" />
+					<img className="corner-bottom-right" src={img} alt="" />
+					<img className="corner-bottom-left" src={img} alt="" />
+					<img className="corner-top-left" src={img} alt="" />
+					<div className="frame-inner">
+						<p className="m-size animated-element">
+							Ми з нетерпінням чекаємо, щоб відсвяткувати разом з вами!
+						</p>
+						<div className="envelope-silver__top-logo animated-element">
+							<span>{envelope.name_1[0]}</span>
+							<span>{envelope.name_2[0]}</span>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
 	);
 };
 
