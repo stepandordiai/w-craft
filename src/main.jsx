98e5f2272b157b61f7f85d11dcb017ccb6282671
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-01 11:35:30 +0200

    Add Helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 // import { StrictMode } from "react";
+import { HelmetProvider } from "react-helmet-async";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
 	// <StrictMode>
-	<App />
+	<HelmetProvider>
+		<App />
+	</HelmetProvider>
 	//  </StrictMode>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { NavLink } from "react-router-dom";
 import data from "./../../assets/data/data.json";
 import Header from "../../components/Header/Header";
@@ -6,6 +7,9 @@ import "./Home.scss";
 const Home = () => {
 	return (
 		<>
+			<Helmet>
+				<title>Весільні сайт-запрошення &sim; pixel flower</title>
+			</Helmet>
 			<Header />
 			<main className="home">
 				{data.map((envelope, index) => {
